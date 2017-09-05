08496c1fd027b1ba7816ed5226014e43168c2c4f
dchao99 <dchao@me.com>
2017-09-05 04:30:11 +0100

    Add files via upload

--- sketch_thingspeak/bq27441gi.cpp
+++ sketch_thingspeak/bq27441gi.cpp
@@ -31,7 +31,6 @@ const unsigned int saved_qmax = 16496;      // Set to -1 if battery data not ava
 uint16 saved_ra_table[] = {67,67,71,82,68,70,84,104,110,116,143,170,301,762,1212};
 #endif //BAT_1
 
-
 // BAT_2 (PKCell_803860): 2000mAh@3.7V Taper=95 Qmax=16572
 #ifdef BAT_2
 const unsigned int design_capacity = 2000;   // (mAh)
@@ -40,7 +39,6 @@ const unsigned int taper_rate = 95;          // = Capacity / (0.1 * Taper curren
 const unsigned int saved_qmax = 16572;       // Set to -1 if battery data not available
 uint16 saved_ra_table[] = {62,62,62,69,50,46,52,57,55,55,70,86,165,424,677};
 #endif //BAT_2
-
 
 // BAT_3 (Panasonic_B-Grn): 3400mAh@3.6V Taper=112 Qmax=16460
 #ifdef BAT_3
@@ -51,7 +49,6 @@ const unsigned int saved_qmax = 16460;       // Set to -1 if battery data not av
 uint16 saved_ra_table[] = {28,284,280,306,210,175,189,195,169,155,200,240,481,1262,2005}; 
 #endif //BAT_3
 
-
 // BAT_4 (Sanyo_GA-Red): 3500mAh@3.6V Taper=113 Qmax=16447
 #ifdef BAT_4
 const unsigned int design_capacity = 3500;   // (mAh)
@@ -59,9 +56,7 @@ const unsigned int design_energy = 3500*3.6; // = Capacity * Nominal Voltage
 const unsigned int taper_rate = 113;         // = Capacity / (0.1 * Taper current)
 const unsigned int saved_qmax = 16447;       // Set to -1 if battery data not available
 uint16 saved_ra_table[] = {24,225,217,238,168,145,159,169,149,140,188,227,452,1183,1887};
-
 #endif //BAT_4
-
 
 // BAT_5 (Samsung_30Q-Pink): 3000mAh@3.6V Taper=110 Qmax=16546
 #ifdef BAT_5
@@ -72,7 +67,6 @@ const unsigned int saved_qmax = 16546;       // Set to -1 if battery data not av
 uint16 saved_ra_table[] = {251,251,233,252,178,157,176,189,172,165,218,267,550,1431,2278};
 #endif //BAT_5
 
-
 // BAT_6 (Samsung_25R-Grn): 2500mAh@3.6V Taper=108 Qmax=16420
 #ifdef BAT_6
 const unsigned int design_capacity = 2500;   // (mAh)
@@ -82,7 +76,6 @@ const unsigned int saved_qmax = 16420;       // Set to -1 if battery data not av
 uint16 saved_ra_table[] = {251,251,238,269,202,187,225,253,231,237,335,415,813,2119,3363};
                        /* {174,174,166,186,136,122,142,155,139,139,193,235,464,1213,1930} */
 #endif //BAT_6
-
 
 // Default Qmax = 16384
 // Default R_a Table = {102,102,99,107,72,59,62,63,53,47,60,70,140,369,588};
